bc40afe7351208ef7125ae9776a95f7ff9ea70f2
brisher777 <BRisher777@gmail.com>
2013-06-02 13:45:49 -0500

    started working on cracking function, and added initial walker function

--- src/PassPro.py
+++ src/PassPro.py
@@ -2,7 +2,14 @@
 from itertools import chain, product
 from optparse import OptionParser
 from string import ascii_lowercase, ascii_uppercase, digits, punctuation
-from timeit import Timer
+from hashlib import new
+from binascii import hexlify
+
+'''
+-- TODO --
+fix crack method (research samdump format)
+-- END --
+'''
 
 class PassPro(object):
     
@@ -61,6 +68,9 @@ class PassPro(object):
         self.parser.add_option('-k', '--walker', dest = 'walker_opts', 
                                help = 'keyboard pattern generator',
                                metavar = 'ARGS')
+        self.parser.add_option('-H', '--cracker', dest = 'hash', 
+                               help = 'hash cracker, uses local word list',
+                               metavar = 'HASH')
         self.parser.add_option('-l', '--length', dest = 'length', 
                                help = 'option of length for generator (required with -g and -c)',
                                metavar = 'NUM')
@@ -76,23 +86,25 @@ class PassPro(object):
         
         (self.opts, self.args) = self.parser.parse_args()
         
-        if self.opts.filename: ## -f 
+        if self.opts.filename: # -f 
             self.read_file(self.opts.filename)
-        elif self.opts.user_input: ## -w
+        elif self.opts.user_input: # -w
             output_object = self.output_words(self.opts.user_input)
             for i in output_object:
                 print ''.join(i)           
-        elif self.opts.gen_opts and self.opts.length: ## -g
+        elif self.opts.gen_opts and self.opts.length: # -g
             gen_obj = self.generator(self.opts.gen_opts, self.opts.length)
             for i in gen_obj:
                 print i    
-        elif self.opts.walker_opts: ## -k 
-            self.walker(self.opts.walker_opts)
-        elif self.opts.customize and self.opts.length: ## -c
+        elif self.opts.walker_opts and self.opts.length: # -k 
+            self.walker(self.opts.walker_opts, self.opts.length)
+        elif self.opts.customize and self.opts.length: # -c
             cust_obj = self.custom(self.opts.customize, self.opts.length)
             for i in cust_obj:
                 print i
-        else: ## no options
+        elif self.opts.hash: # -H
+                self.crack(self.opts.hash)
+        else: # no options
             print self.parser.get_usage()
         
     def read_file(self, args):
@@ -123,12 +135,83 @@ class PassPro(object):
                 chain.from_iterable(product(chars, repeat=i) 
                                               for i in range(1, int(length) + 1)))
             
-    def walker(self, walker_sets):
-        '''  '''
-        pass
+    def walker(self, walker_sets, length):
+        ''' Walks the keyboard in pre-defined patterns with user-defined length '''
+        length = int(length)
+        orig_len = length
+        while length % 4 != 0:
+            length += 1
+        length = length / 4
+        
+        walker = self.alpha_sets.get(walker_sets)
+        temp_list = []
+        try:
+            for i in xrange(len(walker)):
+                for j in xrange(length):
+                    temp_list.append(walker[i + j])
+                word = ''.join(temp_list)
+                print word[:orig_len]
+                temp_list = []   
+        except IndexError:
+            pass
+        
     def output_words(self, user_input):
+        ''' Mangles the word given with the included dictionary '''
         return product(*map(self.alphabet.get, user_input))
     
+    def crack(self, user_input):
+        
+        ''' 
+        need to take a look at lanman hashes and see how they'd be C&P'd into a file.
+        right now the hashes ive used are all below 8 characters because of how 
+        i built them.  Once i know how the lanmans are handled by the samdump, 
+        I can figure out how to process ones that are longer than 8 chars
+        '''
+        
+        word_list = open('ComboFile.txt')
+        print '** If you entered a single hash through the CLI, leave the file entry blank. **'
+        user_file = raw_input('Please specify the file of hashes you\'d like to crack: ')
+        try:
+            input_file = open(user_file)
+            for hash in input_file:
+                hash = hash.strip('\n')
+                for line in word_list:
+                    #print 'line is', line, 'and is', len(line), 'long'
+                    line = line.strip('\n')
+                    if len(line) <= 8:
+                        hashed_pw = hexlify(new('md4', line.encode('utf-16le')).digest()).upper()
+                        if hashed_pw == hash:
+                            print 'The password is', line
+                            word_list.seek(0)
+                            break
+                    else:
+                        first_half = line[:8]
+                        second_half = line[8:]
+                        first_hash = hexlify(new('md4', first_half.encode('utf-16le')).digest()).upper()
+                        second_hash = hexlify(new('md4', second_half.encode('utf-16le')).digest()).upper()
+                        if first_hash == hash or second_hash == hash:
+                            print 'The first part of the password is', first_half
+                            print 'The second part of the password is', second_half
+        except IOError:
+            for line in word_list:
+                line = line.strip('\n')
+                #print 'line is', line, 'and is', len(line), 'long'
+                if len(line) <= 8:
+                    #print '========in the if========'
+                    hashed_pw = hexlify(new('md4', line.encode('utf-16le')).digest()).upper()
+                    if hashed_pw == user_input:
+                        print 'The password is', line
+                else:
+                    #print '********in the else*********'
+                    first_half = line[:8]
+                    second_half = line[8:]
+                    first_hash = hexlify(new('md4', first_half.encode('utf-16le')).digest()).upper()
+                    second_hash = hexlify(new('md4', second_half.encode('utf-16le')).digest()).upper()
+                    if first_hash == user_input or second_hash == user_input:
+                        print 'The first part of the password is', first_half
+                        print 'The second part of the password is', second_half
+                        
+        word_list.close()
     
         
 password = PassPro()
